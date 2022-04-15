112a18fde09df0422fcaa8fc2498d12c33b919ac
Ian Cannon <ian.cannon@protonmail.com>
2022-04-15 13:33:03 -0700

    Refactor Code into separate component, add basic CSS

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import { useEffect, useState } from 'react';
+import Code from './components/Code.jsx';
 
 export default function App(){
 
-const [code, setCode] = useState([]);
-
-  useEffect(() => {
-    const getCode = async () => {
-      const codeAPI = await fetch(`https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new`);
-      let codeText = await codeAPI.text();
-      let codeArray = codeText.split("\n", 4);
-      setCode(codeArray);
-				};
-    getCode(); 
-  },[]);
-
-console.log('the code is ', typeof code, code);
-
+  return(
+    <div className="App"><Code/></div>
+	 );
 }
---
+++ src/components/Code.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import './Code.css';
+
+export default function Code(){
+
+const [codePegs, setCodePegs] = useState([]);
+
+  useEffect(() => {
+    const getCode = async () => {
+      const codeAPI = await fetch(`https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new`);
+      let codeText = await codeAPI.text();
+      let codeArray = codeText.split("\n", 4);
+      setCodePegs(codeArray);
+				};
+    getCode(); 
+  },[]);
+
+  console.log('the code is ', codePegs);
+
+  return(
+    <div className="Code">
+      {codePegs.map((peg) => {
+        return(
+          <div className="Peg">{peg}</div>
+        )
+      })}
+    </div>
+  );
+
+}
